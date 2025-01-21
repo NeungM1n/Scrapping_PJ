6d2ff3d433cfec06c717c161699046137354c4fd
dHyun <sessak0315@gmail.com>
2025-01-21 23:50:13 +0900

    make soup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(3)
+# 평택 내 초등학교 급식 정보를 가져오는 정적 스크래핑
+# 2025.01.21. created by dHyun
+
+# 1. 세교초등학교
+
+import urllib.request as req
+from bs4 import BeautifulSoup
+
+url = "https://sekyo-e.goept.kr/sekyo-e/ad/fm/foodmenu/selectFoodMenuView.do?mi=2520"
+date = "2025-12-02"
+response = req.urlopen(url+date)
+soup = BeautifulSoup(response, "html.parser")
+print(soup)
