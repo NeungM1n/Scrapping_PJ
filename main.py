eece5e80b21414446b11ff8aad30557aa0a200ef
dHyun <sessak0315@gmail.com>
2025-01-23 21:16:51 +0900

    요일별 스크래핑 함함

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # 평택 내 초등학교 급식 정보를 가져오는 정적 스크래핑
 # 2025.01.21. created by dHyun
 
+from bs4 import BeautifulSoup
+import urllib.request as req
+import requests
+requests.packages.urllib3.disable_warnings()
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 # 1. 세교초등학교
 
-import urllib.request as req
-from bs4 import BeautifulSoup
+baseURL = "https://school.koreacharts.com/school/meals/B000009412/contents.html"
+res = req.urlopen(baseURL)
+soup = BeautifulSoup(res, "html.parser")
+meals = soup.find_all(class_="text-center")
 
-url = "https://sekyo-e.goept.kr/sekyo-e/ad/fm/foodmenu/selectFoodMenuView.do?mi=2520"
-date = "2025-12-02"
-response = req.urlopen(url+date)
-soup = BeautifulSoup(response, "html.parser")
-print(soup)
+target = "요일"
+for i, meal in enumerate(meals[28:-3], start=30):  # 인덱스를 1부터 시작
+    text = meal.text.strip()
+    print(text)
+
+    if target in text:
+        print("-" * 50)
